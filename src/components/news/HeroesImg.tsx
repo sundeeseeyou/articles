f052e602c40e5acf99ccf476feddef9c927d6d92
Sandi Miftah <sandiseeyou@gmail.com>
2025-09-23 17:29:05 +0700

    feat: fix spacing

--- src/components/news/HeroesImg.tsx
+++ src/components/news/HeroesImg.tsx
@@ -1,10 +1,10 @@
 import { DUMMY_NEWS } from "../../../dummy-news";
 import Image from "next/image";
 
-export default function HeroesImg() {
+export default function heroesImg() {
   return DUMMY_NEWS.map((items) => (
     <div
-      className="relative w-full h-96 rounded-xl overflow-hidden mb-8"
+      className="relative lg:w-screen h-96 rounded-xl overflow-hidden mb-8"
       key={items.id}
     >
       <Image
@@ -13,7 +13,7 @@ export default function HeroesImg() {
         width={800}
         height={300}
       />
-      <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="absolute inset-0 bg-black/70 flex items-center justify-center">
         <h1 className="text-white text-4xl font-bold">{items.title}</h1>
       </div>
     </div>
