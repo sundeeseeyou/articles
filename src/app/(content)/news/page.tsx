5c02f4671d3093d17418c950be64d2c114bf2033
Sandi Miftah <sandiseeyou@gmail.com>
2025-11-19 19:00:59 +0700

    feat(add): adding database ability to the project

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -1,8 +1,35 @@
 import NewsCard from "@/components/news/NewsCard";
-import { getAllNews } from "@/lib/news";
+// import { getAllNews } from "@/lib/news";
+import { useState, useEffect } from "react";
 
 export default function News() {
-  const all = getAllNews();
+  // const all = getAllNews();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [newsData, setNewsData] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      const response = await fetch("https://localhost:9090");
+
+      if (!response.ok) {
+        setError("Failed to fetch news");
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNewsData(news);
+    }
+    fetchNews();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  let newsContent;
+
+  if (newsData) return (newsContent = <NewsCard news={newsData} />);
   return (
     <>
       <h1 className="text-4xl font-bold">Hottest News!</h1>
