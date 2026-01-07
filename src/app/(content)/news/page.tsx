82bc7c6f1ba4a0f536ef9497f87528c09c9c8ba1
Sandi Miftah <sandiseeyou@gmail.com>
2026-01-07 17:16:12 +0700

    feat(add): fix skeleton

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -20,13 +20,12 @@ function NewsGridSkeleton() {
     <div className="grid lg:grid-cols-4 grid-cols-1 gap-x-8 gap-y-8 justify-center h-full">
       {[...Array(8)].map((_, index) => (
         <div key={index}>
-          <div className="flex flex-col gap-4 justify-center items-center text-black rounded-sm overflow-hidden bg-white p-4 h-full animate-pulse">
+          <div className="flex flex-col gap-4 justify-center items-center text-black rounded-sm overflow-hidden bg-[#a8a8a8] p-4 h-full animate-pulse">
             <div className="w-[300px] h-[300px] bg-gray-300/50 rounded" />
             <div className="w-full space-y-2">
-              <div className="h-5 bg-gray-300/5 rounded w-3/4 mx-auto" />
-              <div className="h-5 bg-gray-300 rounded w-1/2 mx-auto" />
+              <div className="h-5 bg-[#090909] rounded w-3/4 mx-auto" />
             </div>
-            <div className="h-3 bg-gray-300 rounded w-32" />
+            <div className="h-3 bg-[#090909] rounded w-32" />
           </div>
         </div>
       ))}
